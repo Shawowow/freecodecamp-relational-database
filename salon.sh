b45348c9fc5efaf95de010e31fa226c5b7768243
Shawowow <43312292+Shawowow@users.noreply.github.com>
2023-01-31 17:12:12 +0000

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 PSQL="psql --tuples-only --username=freecodecamp --dbname=salon -c "
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
-echo -e "\nWelcome to My Salon, how can I help you?\n"
-MAIN_MENU() {
+echo -e "Welcome to My Salon, how can I help you?\n"
+
+DISPLAY() {
   $PSQL "SELECT * FROM services;" | while IFS=" | " read SERVICE_ID SERVICE_NAME
   do
-    if [[ ! -z $SERVICE_ID ]]
+    if [[ ! -z $SERVICE_ID && ! -z $SERVICE_NAME ]]
     then
-      echo -e "$SERVICE_ID) $SERVICE_NAME"
+      echo  "$SERVICE_ID) $SERVICE_NAME"
     fi
   done
-  read SERVICE_ID_SELECTED
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[1-5]$ ]]
-  then 
-    echo -e "\n"I could not find that service. What would you like today?""
-    MAIN_MENU 
-  fi  
+}
+MAIN_MENU() {
+  
+  until [[ $SERVICE_ID_SELECTED =~ ^[1-5]$ ]]
+  do 
+    read SERVICE_ID_SELECTED
+    echo -e "\nI could not find that service. What would you like today?"
+    DISPLAY 
+  done  
   echo -e "\nWhat's your phone number?"
   read CUSTOMER_PHONE
   CUSTOMER=$($PSQL "select * from customers where phone='$CUSTOMER_PHONE'")
@@ -33,4 +37,5 @@ MAIN_MENU() {
   $PSQL "insert into appointments(customer_id,service_id,time) values($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')"
   echo -e "\nI have put you down for a$SERVICE at $SERVICE_TIME,$CUSTOMER_NAME."
 }
+DISPLAY
 MAIN_MENU
